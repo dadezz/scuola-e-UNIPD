ba4b7000ee98e2b349e0ac682a9607da8074ba38
davide zambon <dadezz@protonmail.com>
2022-01-04 15:23:06 +0100

    Create es1.py

---
+++ uni/1. esercizi vari iniziali/es1.py
@@ -0,0 +1,8 @@
+#anni luce
+
+distanza = 40.7
+distanza_km = distanza*149600000
+velocita = 299800
+tempo = distanza_km/velocita
+print("ci mette circa", int(tempo), "secondi")
+print("che equivalgono circa a", int(tempo/3600), "ore")
